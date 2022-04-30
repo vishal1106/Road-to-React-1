b6842221a3ef76c340538d4d73905800b600b81b
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2022-04-30 14:45:25 +0530

    road to react

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+function navbar(){
+    return `
+    <h3>
+            <a href="/">Home</a>
+        </h3>
+        <div id="options">
+            <h3>
+                <a href="jewelery.html">Jewelery</a>
+            </h3>
+            <h3>
+                <a href="electronics.html">Electronic</a>
+            </h3>
+            <h3>
+                <a href="/">LogIn</a>
+            </h3>
+            <h3>
+                <a href="/">SignUp</a>
+            </h3>
+        </div>
+        `;
+}
+
+export default navbar;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import navbar from "../components/navbar";
+
+
+
+import "../style/navbar.css";
+
+
+let navbar_div=document.getElementById("navbar")
+navbar_div.innerHTML=navbar();
